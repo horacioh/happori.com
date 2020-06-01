096cb701c72bdb8344eda8fec5d7af9b271a0760
Horacio Herrera <hi@horacioh.com>
2020-06-01 02:36:31 +0200

    fix ssr component

--- site/gatsby-ssr.js
+++ site/gatsby-ssr.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./src/styles/main.css";
+import { AppProviders } from "./src/components/app-providers";
 
 export function wrapRootElement({ element }) {
   return <AppProviders>{element}</AppProviders>;
