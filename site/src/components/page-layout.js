b581baa9fab81dc202d29f172f5fce5953d3f1b9
Horacio Herrera <hi@horacioh.com>
2020-06-22 01:52:04 +0200

    new page changes

--- site/src/components/page-layout.js
+++ site/src/components/page-layout.js
@@ -3,7 +3,10 @@ import { MDXProvider } from "@mdx-js/react";
 import { CovidCallout } from "./callout";
 import { MainNav } from "./page-menu";
 import { AboutHeader } from "./about-header";
-import { ContactHeader } from "./contact-header";
+import { TitleSection } from "./contact-header";
+import { Heading1 } from "./headings";
+import { Link } from "./link";
+import { Instagram, Twitter, Facebook } from "react-feather";
 
 export function Container(props) {
   return <div className="mx-auto w-full max-w-4xl" {...props} />;
@@ -17,10 +20,36 @@ export function Section({ className, children, ...rest }) {
   );
 }
 
+export function SocialMedia() {
+  return (
+    <div className="py-4 rounded flex items-center">
+      <Link
+        to="https://instagram.com/happori_com"
+        className="text-primary hover:text-secondary transition duration-200 mr-8"
+      >
+        <Twitter color="currentColor" />
+      </Link>
+      <Link
+        to="https://instagram.com/happori_com"
+        className="text-primary hover:text-secondary transition duration-200 mr-8"
+      >
+        <Instagram color="currentColor" />
+      </Link>
+      <Link
+        to="https://instagram.com/happori_com"
+        className="text-primary hover:text-secondary transition duration-200 mr-8"
+      >
+        <Facebook color="currentColor" />
+      </Link>
+    </div>
+  );
+}
+
 const components = {
   Container,
   Section,
   CovidCallout,
+  SocialMedia,
   p: (props) => (
     <p
       className="mt-10 first:mt-0 w-full max-w-2xl mb-0 text-xl font-light text-primary-hover"
@@ -28,6 +57,7 @@ const components = {
     />
   ),
   strong: (props) => <span className="font-bold" {...props} />,
+  a: (props) => <Link {...props} />,
 };
 
 export function MainLayout({ children }) {
@@ -50,11 +80,40 @@ export function AboutLayout({ children }) {
   );
 }
 
-export function ContactLayout({ children }) {
+export function PageLayout({ children, pageContext: { frontmatter } }) {
+  const { title } = frontmatter;
   return (
     <MDXProvider components={components}>
       <CovidCallout />
-      <ContactHeader />
+      <TitleSection>
+        <Heading1>
+          <span className="text-primary-hover">{title}</span>
+        </Heading1>
+      </TitleSection>
+      <Section>{children}</Section>
+      <PageFooter />
+    </MDXProvider>
+  );
+}
+
+export function ContactLayout({ children, ...props }) {
+  console.log("ContactLayout -> props", props);
+  return (
+    <MDXProvider components={components}>
+      <CovidCallout />
+      <TitleSection>
+        <Heading1>
+          <span
+            className="text-2xl block text-primary"
+            style={{ fontWeight: "300" }}
+          >
+            Cont&aacute;ctanos
+          </span>
+          <span className="text-primary-hover">
+            ¿Qu&eacute; necesitas de nosotos?
+          </span>
+        </Heading1>
+      </TitleSection>
       <Section>{children}</Section>
       <PageFooter />
     </MDXProvider>
