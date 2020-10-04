e197e524ea5c780e0f067ad100b09647ba8b100f
Horacio Herrera <hi@horacioh.com>
2020-10-04 18:53:58 +0200

    remove workspace

---
+++ .cache/async-requires.js
@@ -0,0 +1,16 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-app-js": () => import("./../../../src/pages/app.js" /* webpackChunkName: "component---src-pages-app-js" */),
+  "component---src-pages-checkout-js": () => import("./../../../src/pages/checkout.js" /* webpackChunkName: "component---src-pages-checkout-js" */),
+  "component---src-pages-contacto-mdx": () => import("./../../../src/pages/contacto.mdx" /* webpackChunkName: "component---src-pages-contacto-mdx" */),
+  "component---src-pages-gracias-mdx": () => import("./../../../src/pages/gracias.mdx" /* webpackChunkName: "component---src-pages-gracias-mdx" */),
+  "component---src-pages-houston-mdx": () => import("./../../../src/pages/houston.mdx" /* webpackChunkName: "component---src-pages-houston-mdx" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-landing-js": () => import("./../../../src/pages/landing.js" /* webpackChunkName: "component---src-pages-landing-js" */),
+  "component---src-pages-nosotros-mdx": () => import("./../../../src/pages/nosotros.mdx" /* webpackChunkName: "component---src-pages-nosotros-mdx" */),
+  "component---src-templates-single-product-js": () => import("./../../../src/templates/single-product.js" /* webpackChunkName: "component---src-templates-single-product-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,19 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/.cache/dev-404-page.js"))),
+  "component---src-pages-app-js": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/pages/app.js"))),
+  "component---src-pages-checkout-js": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/pages/checkout.js"))),
+  "component---src-pages-contacto-mdx": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/pages/contacto.mdx"))),
+  "component---src-pages-gracias-mdx": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/pages/gracias.mdx"))),
+  "component---src-pages-houston-mdx": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/pages/houston.mdx"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/pages/index.js"))),
+  "component---src-pages-landing-js": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/pages/landing.js"))),
+  "component---src-pages-nosotros-mdx": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/pages/nosotros.mdx"))),
+  "component---src-templates-single-product-js": hot(preferDefault(require("/Users/horacio/workspace/happori/happori.com/src/templates/single-product.js")))
+}
+
